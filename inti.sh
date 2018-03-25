0de9c8f75dae677643620390c803c2d2c2ac092b
Aitthi Arsa <meanstack20@gmail.com>
2018-03-25 18:20:02 +0700

    Create inti.sh

---
+++ inti.sh
@@ -0,0 +1,23 @@
+sudo su
+aptitude update
+rm /var/lib/dpkg/lock
+dpkg --configure -a
+apt-get update
+apt-get dist-upgrade
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+apt-get install -y nodejs
+git clone https://github.com/vladikoms/WiringOP-Zero.git
+cd WiringOP-Zero
+chmod +x ./build
+bash ./build
+cd ../
+rm -rf ./WiringOP-Zero
+echo "##### Test GPIO #######"
+gpio readall
+echo "exit root"
+exit
+echo "Change npm's Default Directory"
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.profile
+source ~/.profile
